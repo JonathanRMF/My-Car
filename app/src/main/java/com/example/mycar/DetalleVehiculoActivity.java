76fbda80d72a09914650d7addc33021437de0597
agoscollado <youremail.com>
2026-05-18 20:33:46 -0300

    form de alquiler y boton de alquilar

--- app/src/main/java/com/example/mycar/DetalleVehiculoActivity.java
+++ app/src/main/java/com/example/mycar/DetalleVehiculoActivity.java
@@ -3,11 +3,13 @@ package com.example.mycar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Button;
 
 public class DetalleVehiculoActivity extends Activity {
     TextView txtNombreDetalle;
     TextView txtPrecioDetalle;
-
+    Button btnAlquilar;
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +18,20 @@ public class DetalleVehiculoActivity extends Activity {
 
         txtNombreDetalle = findViewById(R.id.txtNombreDetalle);
         txtPrecioDetalle = findViewById(R.id.txtPrecioDetalle);
+        btnAlquilar = findViewById(R.id.btnAlquilar);
 
         String nombre = getIntent().getStringExtra("nombre");
         double precio = getIntent().getDoubleExtra("precio", 0);
 
-        txtPrecioDetalle.setText(nombre);
+        txtNombreDetalle.setText(nombre);
         txtPrecioDetalle.setText("Precio por dia: $" + precio);
+
+        btnAlquilar.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FormularioAlquilerActivity.class);
+            intent.putExtra("nombre", nombre);
+
+            startActivity(intent);
+
+        });
     }
 }
